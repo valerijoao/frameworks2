87d3e2ae570b97f4f37bf32011174dffd97badc9
Marcelo Tomporoski Perez <159451183+Marcelo-IFMS@users.noreply.github.com>
2024-09-23 15:06:00 +0000

    criado os modulos

---
+++ src/modulos/footer.js
@@ -0,0 +1,4 @@
+function Footer() {
+    return<div>IFMS - Dourados</div>    
+}
+export default Footer;
---
+++ src/modulos/navbar.js
@@ -0,0 +1,4 @@
+function Navbar() {
+    return<div>Menu</div>    
+}
+export default Navbaar;
