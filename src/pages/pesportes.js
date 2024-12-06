cb95ce9794061373f2d1196ac8be7c438a131a16
João Valerio <102620622+valerijoao@users.noreply.github.com>
2024-12-06 08:24:04 -0300

    update 06-12

--- src/pages/pesportes.js
+++ src/pages/pesportes.js
@@ -1,4 +1,4 @@
-import { Container } from "react-bootstrap";
+import { Container, Table} from "react-bootstrap";
 import Menu from "./modulos/navbar";
 import Footer from "./modulos/footer";
 
@@ -10,7 +10,28 @@ export default function Pesportes() {
                 Lista de Produtos de Esportes
             </Container>
             <Container>
-                Os Produtos
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Produto</th>
+                            <th>Categoria</th>
+                            <th>Descrição</th>
+                            <th>Preço</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                       {/* laço for  */}
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+
+                    </tbody>
+                </Table>
             </Container>
         </Container>
         <Footer />
